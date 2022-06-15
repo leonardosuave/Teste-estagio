bfee91a0afeb4014161de2a6231a3dfcea7eebad
Leonardo Suave <105296883+leonardosuave@users.noreply.github.com>
2022-06-15 20:22:29 -0300

    atualização de teste

---
+++ ex1.js
@@ -0,0 +1,11 @@
+function SOMA() {
+    let Indice = 13;
+    let Soma = 0;
+    for(let k = 0; k < Indice; ++k) {
+        Soma = Soma + k
+    }
+    return(Soma)
+};
+console.log(SOMA());
+//O valor da SOMA é 78
+
